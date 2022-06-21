e6d9b8b9a5761ef0efb0b3e0fb2b03adbdda0029
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-22 00:05:28 +0200

    Added body parser

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
+const body_pars = require("body-parser")
 const app = express();
 const routing = require("./routing_sys.js");
 require('dotenv').config()
 
-app.use((req, res, next) => {
-    console.log(`New request`);
-    next();
-});
+app.use(body_pars.json())
 
-app.use('',routing)
+app.use('', routing)
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT, function() {
     console.log(`App is running on ${process.env.PORT}`);
     console.log(`We are currently in a ${process.env.STATE} enviorment`)
 });
--- routing_sys.js
+++ routing_sys.js
@@ -1,12 +1,12 @@
 module.exports = (function() {
     var route = require('express').Router();
 
-    route.get('/', function (req, res,next) {
+    route.get('/', function(req, res, next) {
         res.send('Welcome to my very awesome thing');
     });
 
-    route.get('/Animal/:type', function (req, res,next) {
-        if(req.params.type == 0) {
+    route.post('/Animal/:type', function(req, res, next) {
+        if (req.params.type == 0) {
             next('route');
         }
         res.send(`You gave us ${req.params.type}`)
