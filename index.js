50cd568f31683fc668991119f13417d96263cb2b
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-22 02:27:37 +0200

    Added rate limit

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const app = express();
 const routing = require("./routing_sys.js");
 const rateLimit = require('express-rate-limit')
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 20,
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
+
 require('dotenv').config()
 
 app.use(body_pars.json())
+
+app.use('', apiLimiter)
 
 app.use('', routing)
 
