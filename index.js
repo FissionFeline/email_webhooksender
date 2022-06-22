1b72258f7c34bf9f152a2e368715b745f5cd5ae6
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-22 02:24:19 +0200

    no more nodemailer

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ const express = require('express');
 const body_pars = require("body-parser")
 const app = express();
 const routing = require("./routing_sys.js");
-const nmailer = require('nodemailer')
 
 require('dotenv').config()
-
-var mail_trans = nmailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.mail_user,
-        pass: process.env.pass_pass
-    }
-});
 
 app.use(body_pars.json())
 
