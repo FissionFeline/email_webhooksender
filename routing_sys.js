19998b38d707a740d30a7846af8925e00c16b51e
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-06-24 15:42:08 +0200

    Removed old code lul

--- routing_sys.js
+++ routing_sys.js
@@ -17,13 +17,6 @@ module.exports = (function() {
             if (error) next()
         });
     });
-
-    route.post('/Animal/:type', function(req, res, next) {
-        if (req.params.type == 0) {
-            next('route');
-        }
-        res.send(`You gave us ${req.params.type}`)
-    });
     route.use((req, res, next) => {
         res.send("Give us sth will ya ?");
     });
